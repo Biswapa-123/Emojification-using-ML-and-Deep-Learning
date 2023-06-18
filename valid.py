5414cafc99454a7b84cb5ab32ec63e458578fbd0
Biswapa-123 <136886112+Biswapa-123@users.noreply.github.com>
2023-06-18 23:55:33 +0530

    Create valid.py

---
+++ valid.py
@@ -0,0 +1,18 @@
+train_dir = 'data/train'
+val_dir = 'data/test'
+train_datagen = ImageDataGenerator(rescale=1./255)
+val_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="gray_framescale",
+        class_mode='categorical')
+
+validation_generator = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(48,48),
+        batch_size=64,
+        color_mode="gray_framescale",
+        class_mode='categorical')
